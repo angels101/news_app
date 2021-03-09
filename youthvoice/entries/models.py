3357859241eacd2084d04c244691c288a34d6e4c
angels101 <angelscodex101@gmail.com>
2021-03-09 17:13:49 +0300

    Apply all migrations: admin, auth, contenttypes, entries, sessions

--- youthvoice/entries/models.py
+++ youthvoice/entries/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class entry(models.Model):
+    entry_title = models.CharField(max_length=50)
+    entry_text = models.TextField()
+    entry_data = models.DateTimeField(auto_now_add=True)
+    entry_author = models.ForeignKey(User,on_delete=models.CASCADE)
