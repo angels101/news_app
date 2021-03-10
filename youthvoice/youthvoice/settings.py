fa637b827e5aedd744269ca87e074876908fc83e
angels101 <angelscodex101@gmail.com>
2021-03-10 11:01:22 +0300

    templates

--- youthvoice/youthvoice/settings.py
+++ youthvoice/youthvoice/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+#STATIC_ROOT = os.path.join(BASE_DIR/ 'static')
+#TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'entries',
+    'entries.apps.EntriesConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR =[
+    BASE_DIR / "static",
+    '/youthvoice/static',
+]
